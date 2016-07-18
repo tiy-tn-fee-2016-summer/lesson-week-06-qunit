50de333711c7ed49a48707ee97ea6b65fbb96b42
Ryan Tablada <ryan.tablada@gmail.com>
2016-07-18 11:45:34 -0500

    Dream inside of a dream

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
 'use strict';
+var inIndex = 'Foo';
+
+export default function() {
+  console.log(inIndex);
+
+  (function() {
+    console.log('I can still use inIndex here', inIndex);
+  })();
+}
